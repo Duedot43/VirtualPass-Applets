0a6b7af57a61fb2547f68c94ad08859f2f0ce1aa
Duedot43 <jackgendill@icloud.com>
2022-05-23 20:47:29 -0600

    Soimthinbgw[poih3r;oih3ihq['oq['gfoubqruoqnrg

--- Plugins/general_checkout/new_files/gen_checkout/index.php
+++ Plugins/general_checkout/new_files/gen_checkout/index.php
@@ -1,5 +1,20 @@
 <?php
-
+if (isset($_POST['id']) and isset($_GET['room'])){
+    $mass_json = json_decode(file_get_contents("../../mass.json"), true);
+    foreach ($mass_json['user'] as $user_id){
+        $user_ini = parse_ini_file("../registered_phid/" . $user_id);
+        if ($user_ini['student_id'] == $_POST['id']){
+            $user = $user_ini;
+            break;
+        }
+    }
+    echo "Welcome " . $user['first_name'] . ". You have been regustered into this area.
+    <script>
+    setTimeout(() => { link('/gen_checkout/index.php?room=" . $_GET['room'] . "') }, 4000);
+    </script>";
+    $gen_index = json_decode(file_get_contents("../../gen_config/gen_index.json"), true);
+    $gen_index['rooms'][$_GET['room']] = $something;
+}
 
 ?>
 <title>Login</title>
